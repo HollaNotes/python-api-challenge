acf33dac6bb420c983750d9c0856fc1259a63762
HollaNotes <noellewidick@gmail.com>
2023-09-29 16:51:43 -0700

    saving

--- api_keys_NoelleWatson.py
+++ api_keys_NoelleWatson.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "14c61fbce5102e43ae887b2fa21404cc"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "ca21aacdfeae48f1a9ebe1771570db2f"
